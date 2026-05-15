8ccc2fae5c563d278a7e2bf599f51470ce1c0711
John Lewis <github@jlewis.sh>
2026-05-15 10:25:06 -0700

    feat: add tracing

--- crates/cambo/src/main.rs
+++ crates/cambo/src/main.rs
@@ -1,4 +1,8 @@
 use miette::{Context, IntoDiagnostic};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{
+  EnvFilter, layer::SubscriberExt, util::SubscriberInitExt,
+};
 use winit::event_loop::EventLoop;
 
 use self::{gpu_context::GpuContext, window_app::App};
@@ -9,6 +13,16 @@ mod window_state;
 
 fn main() -> miette::Result<()> {
   let gpu = GpuContext::new()?;
+  tracing_subscriber::registry()
+    .with(tracing_subscriber::fmt::layer())
+    .with(
+      EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy(),
+    )
+    .try_init()
+    .into_diagnostic()?;
+
   let event_loop = EventLoop::new()
     .into_diagnostic()
     .context("failed to build winit event loop")?;
